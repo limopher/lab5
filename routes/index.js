6088858d40810a94dce7d6ddf8711de853cb889e
Chris Lim <cklim@ucsd.edu>
2016-02-04 20:26:32 +0000

    putting it together lab

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,32 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index');
+	res.render('index', {
+		'friends':[
+			{ 'name': 'Doug Engelbar',
+			  'description': 'Has awesome pet mice',
+			  'imageURL': 'http://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Douglas_Engelbart_in_2008.jpg/972px-Douglas_Engelbart_in_2008.jpg'
+			},
+			{ 'name': 'Ivan Sutherland',
+			  'description': 'Great at sketching',
+			  'imageURL': 'http://upload.wikimedia.org/wikipedia/commons/5/5c/Ivan_Sutherland_at_CHM.jpg'									
+			},
+			{ 'name': 'Lucy Suchman',
+			  'description': 'Loves Trukese navigators',
+			  'imageURL': 'http://upload.wikimedia.org/wikipedia/commons/d/df/Lucy_Suchman.jpeg'			
+			},
+			{ 'name': 'Vannevar Bush',
+			  'description': 'Has stellar associative memory',
+			  'imageURL': 'http://upload.wikimedia.org/wikipedia/commons/e/ea/Vannevar_Bush_portrait.jpg'			
+			},
+			{ 'name': 'Grace Hopper',
+			  'description': 'Only friend with a Navy ship named after them',
+			  'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Grace_Hopper.jpg/220px-Grace_Hopper.jpg'
+			},
+			{ 'name': 'Allen Newell',
+			  'description': 'Really into psychology',
+			  'imageURL': 'http://amturing.acm.org/images/lg_aw/3167755.jpg'
+			}		
+		  ] 
+	});
 };
